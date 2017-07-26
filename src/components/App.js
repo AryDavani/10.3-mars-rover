2288a53fa700a0d9e5f1602e6bc45fd0166fc434
Ary Davani <arydavani@gmail.com>
2017-07-26 14:49:53 -0400

    form updating state

--- src/components/App.js
+++ src/components/App.js
@@ -14,11 +14,28 @@ class App extends Component {
       sol: ''
     }
 
+    this._handleChange = this._handleChange.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
+
     let rover = this.state.rover;
     let camera = this.state.camera;
     let number = this.state.sol;
 
     const IMG_URL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${number}&camera=${camera}&api_key=${API_KEY}';
+  }
+
+  _handleChange(event){
+    console.log(event.target.name);
+    console.log(event.target.value);
+
+    let object = {};
+    object[event.target.name] = event.target.value;
+    this.setState(object);
+  }
+
+  _handleSubmit(event){
+    console.log(this.state);
+    console.log("working");
   }
 
   render() {
@@ -27,24 +44,24 @@ class App extends Component {
 
         <form onSubmit={ this._handleSubmit }>
 
-          <label htmlFor="rover">Rover</label>
-          <select onChange={ this._handleChange } id="rover" value={ this.state.value }>
+          <label>Rover</label>
+          <select onChange={ this._handleChange } name="rover" value={this.state.value}>
             <option value="Curiosity">Curiosity</option>
             <option value="Opportunity">Opportunity</option>
             <option value="Spirit">Spirit</option>
           </select>
 
-          <label htmlFor="camera">Camera</label>
-          <select onChange={ this._handleChange } id="camera" value={ this.state.value }>
+          <label>Camera</label>
+          <select onChange={ this._handleChange } name="camera" value={ this.state.value }>
             <option value="fhaz">FHAZ (Front Hazard)</option>
             <option value="rhaz">RHAZ (Rear Hazard)</option>
             <option value="navcam">NAVCAM (Navigation Cam)</option>
           </select>
 
-          <label htmlFor="sol">Martian Sol: 1000-2000</label>
-          <input type="number" onChange={ this._handleChange } max="2000" min="1000" value={ this.state.value }/>
+          <label>Martian Sol: 1000-2000</label>
+          <input name="sol" type="number" onChange={ this._handleChange } max="2000" min="1000" value={ this.state.value }/>
 
-          <input type="submit" onChange={ this._handleChange} value="Get Rover Images" />
+          <input type="submit" value="Get Rover Images" />
         </form>
 
       </div>
