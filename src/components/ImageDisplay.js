74505e7eeda09ebd7bf9ff57af5e4b8ee91fb980
Ary Davani <arydavani@gmail.com>
2017-08-17 16:05:13 -0400

    added background

--- src/components/ImageDisplay.js
+++ src/components/ImageDisplay.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function ImageDisplay(props) {
   return (
-    <div>
+    <div className="images">
       <img src={ props.image.img_src } alt="mars rover images" />
     </div>
   )
