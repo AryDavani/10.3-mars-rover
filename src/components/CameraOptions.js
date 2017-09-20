057bf7d19fa9cfaaed8ba60f0f5bcd18cf6f045b
Ary Davani <arydavani@gmail.com>
2017-09-20 11:42:53 -0400

    can now get diff cameras per rover

--- src/components/CameraOptions.js
+++ src/components/CameraOptions.js
@@ -9,23 +9,28 @@ export default function CameraOptions(props) {
         <option value="mast">Mast Camera</option>
         <option value="chemcam">Chemistry and Camera Complex</option>
         <option value="mahli">Mars Hand Lens Imager</option>
-        <option value="mahli">Mars Hand Lens Imager</option>
+        <option value="mardi">Mars Decent Imager</option>
+        <option value="navcam">Navigation Camera</option>
       </select>
     )
   } else if (props.state.rover === 'Opportunity') {
     return (
       <select className="browser-default black-text" name="camera" value={ props.state.camera } onChange={ props.handleChange }>
-        <option value="Curiosity">asdlkfja;sldfj</option>
-        <option value="Opportunity">;lsjdfla</option>
-        <option value="Spirit">ljasd;fjl;asfj</option>
+        <option value="fhaz">Front Hazard Avoidance Camera</option>
+        <option value="rhaz">Rear Hazard Avoidance Camera</option>
+        <option value="navcam">Navigation Camera</option>
+        <option value="pancam">Panoramic Camera</option>
+        <option value="minites">Miniature Thermal Emission Spectrometer</option>
       </select>
     )
   } else if (props.state.rover === 'Spirit') {
     return (
       <select className="browser-default black-text" name="camera" value={ props.state.camera } onChange={ props.handleChange }>
-        <option value="Curiosity">dddddddd</option>
-        <option value="Opportunity">aaaaaaaaaa</option>
-        <option value="Spirit">ggggggggg</option>
+        <option value="fhaz">Front Hazard Avoidance Camera</option>
+        <option value="rhaz">Rear Hazard Avoidance Camera</option>
+        <option value="navcam">Navigation Camera</option>
+        <option value="pancam">Panoramic Camera</option>
+        <option value="minites">Miniature Thermal Emission Spectrometer</option>
       </select>
     )
   }
--- src/components/blah.js
+++
@@ -1,5 +0,0 @@
-<select className="browser-default black-text" onChange={ this._handleChange } name="camera" value={ this.state.camera }>
-  <option value="fhaz">FHAZ (Front Hazard)</option>
-  <option value="rhaz">RHAZ (Rear Hazard)</option>
-  <option value="navcam">NAVCAM (Navigation Cam)</option>
-</select>
